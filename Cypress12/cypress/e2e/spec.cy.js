49ec1319e501af9c4828430bf1146e6453c4024a
GretaMkrtchyan <152791761+GretaMkrtchyan@users.noreply.github.com>
2024-01-27 22:00:44 +0400

    Add files via upload

---
+++ Cypress12/cypress/e2e/spec.cy.js
@@ -0,0 +1,26 @@
+
+describe('template spec', () => {
+  const randomData = Date.now().toString()
+
+  it('API testing', function() {
+    cy.CreateNewUser(randomData, this.data).then(response => {
+      expect(response.body.email).to.eq("mmnn5677999yan@mail.com"),
+      expect(response.body.name).to.eq("Greta")
+      const user_id = response.body.id 
+   
+     cy.GetList(user_id).then(response => {
+       expect(response.body.email).to.eq("mmnn5677999yan@mail.com"),
+      expect(response.body.name).to.eq("Greta"),
+      expect(response.body.id).to.eq(user_id)
+    })  
+    
+     cy.DeleteUser(user_id).then(response => {
+        expect(response.status).to.eq(204)
+
+})
+})         
+})
+})
+
+
+      
